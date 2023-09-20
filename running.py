0db2f19d9eac87098ccc539ae05cab29332b80a7
imnamejm(Lect) <eileen031029@naver.com>
2023-09-20 17:44:10 +0900

    코드 작성

---
+++ running.py
@@ -0,0 +1,21 @@
+import math
+
+from pico2d import*
+
+open_canvas()
+
+grass=load_image('grass.png')
+character = load_image('character.png')
+
+def run_circcle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
