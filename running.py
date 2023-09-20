7b668461ef894052ab3322953213545e702cc045
imnamejm(Lect) <eileen031029@naver.com>
2023-09-20 18:13:49 +0900

    rander_all함수 생성

--- running.py
+++ running.py
@@ -7,28 +7,30 @@ open_canvas()
 grass=load_image('grass.png')
 character = load_image('character.png')
 
+def rander_all(x, y):
+    clear_canvas.now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.1) 
+
 def run_circcle():
     print('CIRCLE')
     cx , cy, r = 400, 300, 200
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(deg / 360 * 2 * math.pi)
         y = cy + r * math.sin(deg / 360 * 2 * math.pi)
-        clear_canvas.now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)        
+        rander_all(x, y)
 
 def run_rectangle():
     print('RECTANGLE')
 
 #botton line
     for x in range(50, 750,10):
-        clear_canvas.now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)     
+        rander_all(x, 90)
+
+    for x in range (750,50,-10):
+        
     
-    pass
 
 while True:
     run_circle()
