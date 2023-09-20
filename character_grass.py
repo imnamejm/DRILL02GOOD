dbe06cfbae8ba7cac963281716cb8064324243ff
imnamejm(Lect) <eileen031029@naver.com>
2023-09-20 18:29:30 +0900

    fix

--- character_grass.py
+++ character_grass.py
@@ -6,21 +6,6 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-y = 90
 
-while (x < 800):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
-
- while (y < 600):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(798, y)
-    y = y + 2
-    delay(0.01)
 
 close_canvas()
--- running.py
+++ running.py
@@ -8,11 +8,18 @@ grass=load_image('grass.png')
 character = load_image('character.png')
 
 def rander_all(x, y):
-    clear_canvas.now()
+    clear_canvas_now()
     grass.draw_now(400,30)
     character.draw_now(x,y)
     delay(0.1) 
 
+def run_circle():
+    print('CIRCLE')
+    cx , cy, r = 400, 300, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        rander_all(x, y)
 
 def run_rectangle():
     print('RECTANGLE')
