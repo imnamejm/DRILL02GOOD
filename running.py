96923ef9f5e3e218202b31b4d1f5745f93c52308
imnamejm(Lect) <eileen031029@naver.com>
2023-09-20 17:58:01 +0900

    원 그리기 완성
    근데 난 안 그려짐

--- running.py
+++ running.py
@@ -8,12 +8,16 @@ grass=load_image('grass.png')
 character = load_image('character.png')
 
 def run_circcle():
-    clear_canvas.now()
-    grass.draw_now(400,30)
-    character.draw_now(400,9)
-    delay(1)
     print('CIRCLE')
-    pass
+    cx , cy, r = 400, 300, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        clear_canvas.now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)        
+  
 
 def run_rectangle():
     print('RECTANGLE')
